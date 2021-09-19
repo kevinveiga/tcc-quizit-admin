5d37167e285703665427813da6e94f83ac115a23
Kevin Veiga <kevin.veiga@gmail.com>
2021-09-19 15:06:38 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 import * as admin from 'firebase-admin';
 
-admin.initializeApp();
+console.log('init');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://tcc-quizit.firebaseio.com'
+});
+
+async function grantAdminRole(email) {
+    // const user = await admin.auth().getUserByEmail(email);
+
+    // if (user.customClaims && user.customClaims.admin === true) {
+    //     return null;
+    // }
+
+    await admin.auth().setCustomUserClaims('SRWLsnmSPfNdEyGjPeFal6av4t42', {
+        admin: true
+    });
+
+    console.log('grantAdminRole');
+}
+
+grantAdminRole();
+
+console.log('end');
